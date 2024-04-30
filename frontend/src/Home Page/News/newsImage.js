d5b63fe8a572dd809734aa5b353e20b5743fc6f9
samanthacel <samantha.michelle1711@gmail.com>
2024-04-30 20:57:44 +0700

    news section

---
+++ frontend/src/Home Page/News/newsImage.js
@@ -0,0 +1,25 @@
+import news1 from '../../image-assets/newsbanner1.png'
+import news2 from '../../image-assets/newsbanner2.png'
+import news3 from '../../image-assets/newsbanner3.png'
+import news4 from '../../image-assets/newsbanner4.png'
+
+const images = [
+  {
+    imgURL: news1,
+    imgAlt: "img-1"
+  },
+  {
+    imgURL: news2,
+    imgAlt: "img-2"
+  },
+  {
+    imgURL: news3,
+    imgAlt: "img-3"
+  },
+  {
+    imgURL: news4,
+    imgAlt: "img-4"
+  }
+];
+
+export default images;
--- frontend/src/Home Page/homePage.jsx
+++ frontend/src/Home Page/homePage.jsx
@@ -3,13 +3,19 @@ import Footer from "../Universal Components/footer";
 import HomeNavbar from "../Universal Components/navbars/homeNavbar";
 import './homePage.css'
 import News from "./News/news";
+import images from "./News/newsImage";
+
 
 function HomePage() {
   return (
     <>
       <HomeNavbar />
       <div className="homepage-container">
-        <News />
+        <News>
+          {images.map((image, index) => {
+            return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
+          })}
+        </News>
         <Footer />
       </div>
     </>
